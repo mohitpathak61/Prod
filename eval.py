f0060b3fa2cc2e007334a70110d09389a58afd5d
mohit <mohit.pathak@mygurukulam.org>
2019-12-01 22:01:42 +0530

    eval fun

---
+++ eval.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec  1 22:00:16 2019
+
+@author: m
+"""
+
+ # expression to be evaluated
+ 
+expr = raw_input("Enter the function(in terms of x):")
+
+#variable used in expression
+
+x = int(raw_input("Enter the value of x:"))
+  
+# evaluating expression
+
+y = eval(expr)
+  
